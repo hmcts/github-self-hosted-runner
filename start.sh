8e6318cdb0f6a2fa4beddbf8e9f658683f502290
Alex Bance <Alex.Bance@justice.gov.uk>
2024-05-21 14:36:20 +0100

    fix: added label support

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 GH_OWNER=$GH_OWNER
 GH_REPOSITORY=$GH_REPOSITORY
 GH_TOKEN=$GH_TOKEN
+LABELS=$LABELS
 
 RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
 RUNNER_NAME="landing-zone-runner-${RUNNER_SUFFIX}"
@@ -11,7 +12,11 @@ REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Author
 
 cd /home/actions/actions-runner
 
-./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
+if [ -z "$LABELS" ]; then
+    ./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
+else
+    ./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME} --labels ${LABELS}
+fi
 
 cleanup() {
     echo "Removing runner..."
